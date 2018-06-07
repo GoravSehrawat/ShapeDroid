4d4c89759b173b22d0c76cf5c9a122e71c11e4d0
goravsehrawat    git config --global user.name goravsehrawat    git config --global user.name goravsehrawat <goravmcsdu2015@gmail.com>
2018-06-07 09:38:32 +0530

    store files

--- app/src/main/java/com/example/bsw123/shapedroid/MakeShape.java
+++ app/src/main/java/com/example/bsw123/shapedroid/MakeShape.java
@@ -7,6 +7,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by bsw123 on 07/06/18.
@@ -30,13 +34,33 @@ public class MakeShape extends View {
         mPaint.setStyle(Paint.Style.STROKE);
     }
 
+
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.WHITE);
+        ArrayList<RectAngleStore> Store;
+        Store = new ArrayList();
         if (touched) {
+            RectAngleStore object=new RectAngleStore();
             canvas.drawRect(x - width/2, y - height/2, x + width/2, y + height/2, mPaint);
-
+            object.setX1coordinate(x-width/2);
+            object.setX2coordinate(x+width/2);
+            object.setX3coordinate(x+width/2);
+            object.setX4coordinate(x-width/2);
+            object.setY1coordinate(y-height/2);
+            object.setY2coordinate(y-height/2);
+            object.setY3coordinate(y+height/2);
+            object.setY4coordinate(y+height/2);
+            Store.add(object);
         }
+        Iterator<RectAngleStore> iter = Store.iterator();
+        while(iter.hasNext())
+        {
+            RectAngleStore yp = iter.next();
+            yp.printCoordinates();
+        }
+
+
     }
 
     @Override
